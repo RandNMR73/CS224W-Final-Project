848f750196581429399ceb0c48d95b4892175c82
GabeTsai <gthtsai@stanford.edu>
2024-12-07 17:46:43 -0800

    relbench dataset to task map in config

--- config.py
+++ config.py
@@ -1,3 +1,14 @@
-root_dir = ""
-neighbors_per_node = 128
-depth = 2
+ROOT_DIR = ""
+NEIGHBORS_PER_NODE = 128
+DEPTH = 2
+
+#Dictionary that maps RelBench database to their associated tasks
+RELBENCH_DATASETS = {
+    "rel-amazon": ["user-churn", "item-churn", "user-ltv", "item-ltv", "user-item-purchase", "user-item-rate", "user-item-review"], 
+    "rel-stack": ["user-engagement", "user-badge", "post-votes", "user-post-comment", "post-post-related"],
+    "rel-trial": ["study-outcome", "study-adverse", "site-success", "condition-sponsor-run", "site-sponsor-run"],
+    "rel-f1": ["driver-dnf", "driver-top3", "driver-position"], 
+    "rel-hm": ["user-churn", "item-sales", "user-item-purchase"],
+    "rel-event": ["user-repeat", "user-ignore", "user-attendance"],
+    "rel-avito": ["user-visits", "user-clicks", "ad-ctr", "user-ad-visit"] 
+}
